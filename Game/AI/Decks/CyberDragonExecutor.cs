9ab5c106209dba0070bdf2e71b115a5c8bdab23e
VI-1911 <2571845421@qq.com>
2022-11-21 03:29:47 +0800

    .

---
+++ Game/AI/Decks/CyberDragonExecutor.cs
@@ -0,0 +1,294 @@
+﻿using YGOSharp.OCGWrapper.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using WindBot;
+using WindBot.Game;
+using WindBot.Game.AI;
+
+namespace WindBot.Game.AI.Decks
+{
+    // NOT FINISHED YET
+    [Deck("CyberDragon", "AI_CyberDragon", "NotFinished")]
+    public class CyberDragonExecutor : DefaultExecutor
+    {
+        bool PowerBondUsed = false;
+
+        public class CardId
+        {
+            public const int AshBlossomAndJoyousSpring = 14558127;
+            public const int MaxxC = 23434538;
+			
+			public const int NaturalExterio = 99916754;
+            public const int NaturalBeast = 33198837;
+            public const int SwordsmanLV7 = 37267041;
+            public const int RoyalDecreel = 51452091;
+			public const int ImperialOrder = 61740673;
+			
+            public const int CyberDragon = 70095154;
+            public const int CyberDragonVier = 29975188;
+            public const int CyberDragonCore = 23893227;
+			public const int CyberDragonHerz = 56364287;
+			public const int CyberDragonNachster = 1142880;
+			
+			public const int HarpieFeatherDuster = 18144506;
+			public const int CalledByTheGrave = 24224830;
+			public const int ReadyFusion = 63854005;
+            public const int Raigeki = 12580477;
+			public const int lightningStorm = 14532163;
+            public const int Polymerization = 24094653;
+            public const int PowerBond = 37630732;
+			public const int OverloadFusion = 3659803;
+			public const int MachineDuplication = 63995093;
+			public const int CyberEmergency = 60600126;
+			public const int CyberRevsystem = 33041277;
+			public const int LimiterRemoval = 23171610;
+			
+            public const int CyberTwinDragon = 74157028;
+			public const int CyberEndDragon = 1546123;
+			public const int ChimeratechRampageDragon = 84058253;
+            public const int CyberDragonInfinity = 10443957;
+            public const int CyberDragonNova = 58069384;
+			public const int CyberDragonSieger = 46724542;
+        }
+
+        public CyberDragonExecutor(GameAI ai, Duel duel)
+            : base(ai, duel)
+        { 
+			//first do
+			AddExecutor(ExecutorType.Activate, CardId.lightningStorm, DefaultLightingStorm);
+            AddExecutor(ExecutorType.Activate, CardId.HarpieFeatherDuster, DefaultHarpiesFeatherDusterFirst);
+			AddExecutor(ExecutorType.Activate, CardId.Raigeki, DefaultRaigeki);			
+			//counter
+            AddExecutor(ExecutorType.Activate, CardId.CalledByTheGrave, DefaultCalledByTheGrave);
+			//first do	
+			AddExecutor(ExecutorType.SpSummon, CardId.CyberDragon);
+            AddExecutor(ExecutorType.Summon, CardId.CyberDragonCore);
+			AddExecutor(ExecutorType.Activate, CardId.CyberDragonVier);
+			AddExecutor(ExecutorType.Activate, CardId.CyberDragonNachster);
+			
+			AddExecutor(ExecutorType.Summon, CardId.CyberDragonVier);
+			AddExecutor(ExecutorType.MonsterSet, CardId.CyberDragonHerz);
+			AddExecutor(ExecutorType.Summon, CardId.CyberDragonNachster);
+			
+			AddExecutor(ExecutorType.SpSummon, CardId.CyberDragonNova, CyberDragonNovaSummon);
+            AddExecutor(ExecutorType.Activate, CardId.CyberDragonNova, CyberDragonNovaEffect);
+            AddExecutor(ExecutorType.SpSummon, CardId.CyberDragonInfinity, CyberDragonInfinitySummon);
+            AddExecutor(ExecutorType.Activate, CardId.CyberDragonInfinity, CyberDragonInfinityEffect);
+			AddExecutor(ExecutorType.SpSummon, CardId.CyberDragonSieger, CyberDragonSiegerSummon);
+			AddExecutor(ExecutorType.Activate, CardId.CyberDragonSieger);
+			
+            AddExecutor(ExecutorType.Activate, CardId.Raigeki, DefaultRaigeki);
+            AddExecutor(ExecutorType.Activate, CardId.Polymerization, PolymerizationEffect);
+            AddExecutor(ExecutorType.Activate, CardId.PowerBond, PowerBondEffect);
+			AddExecutor(ExecutorType.Activate, CardId.OverloadFusion, OverloadFusionEffect);
+			AddExecutor(ExecutorType.Activate, CardId.MachineDuplication);
+			AddExecutor(ExecutorType.Activate, CardId.CyberEmergency, CyberEmergencyEffect);
+			AddExecutor(ExecutorType.Activate, CardId.CyberRevsystem);
+			AddExecutor(ExecutorType.Activate, CardId.LimiterRemoval, LimiterRemovalEffect);
+			
+			AddExecutor(ExecutorType.Activate, CardId.AshBlossomAndJoyousSpring, DefaultAshBlossomAndJoyousSpring);
+            AddExecutor(ExecutorType.Activate, CardId.MaxxC, DefaultMaxxC);
+			
+            AddExecutor(ExecutorType.Activate, CardId.CyberDragonCore, CyberDragonCoreEffect);
+			AddExecutor(ExecutorType.Activate, CardId.CyberDragonHerz, CyberDragonHerzEffect);
+			AddExecutor(ExecutorType.Activate, CardId.ChimeratechRampageDragon);
+
+            AddExecutor(ExecutorType.SpSummon, CardId.CyberEndDragon);
+            AddExecutor(ExecutorType.SpSummon, CardId.CyberTwinDragon);
+			AddExecutor(ExecutorType.SpSummon, CardId.ChimeratechRampageDragon);
+			
+			AddExecutor(ExecutorType.SpellSet, DefaultSpellSet);
+        }
+
+        private bool CyberDragonInHand()  { return Bot.HasInHand(CardId.CyberDragon); }
+        private bool CyberDragonInGraveyard()  { return Bot.HasInGraveyard(CardId.CyberDragon); }
+        private bool CyberDragonInMonsterZone() { return Bot.HasInMonstersZone(CardId.CyberDragon); }
+        private bool CyberDragonIsBanished() { return Bot.HasInBanished(CardId.CyberDragon); }
+		private bool CyberDragonInfinitySummoned = false;
+
+        public override void OnNewTurn()
+        {
+            CyberDragonInfinitySummoned = false;
+        }
+		
+        private bool PolymerizationEffect()
+        {
+            if (Bot.GetCountCardInZone(Bot.MonsterZone, CardId.CyberDragon) + Bot.GetCountCardInZone(Bot.MonsterZone, CardId.CyberDragonVier) + Bot.GetCountCardInZone(Bot.MonsterZone, CardId.CyberDragonCore) + Bot.GetCountCardInZone(Bot.MonsterZone, CardId.CyberDragonHerz) + Bot.GetCountCardInZone(Bot.MonsterZone, CardId.CyberDragonNachster) + Bot.GetCountCardInZone(Bot.Hand, CardId.CyberDragon) >= 3)
+                AI.SelectCard(CardId.CyberEndDragon);
+            else 
+				AI.SelectCard(CardId.CyberTwinDragon);
+            return true;
+        }
+
+        private bool PowerBondEffect()
+        {
+            PowerBondUsed = true;
+            if ((Bot.GetCountCardInZone(Bot.MonsterZone, CardId.CyberDragon) + Bot.GetCountCardInZone(Bot.MonsterZone, CardId.CyberDragonVier) + Bot.GetCountCardInZone(Bot.MonsterZone, CardId.CyberDragonCore) + Bot.GetCountCardInZone(Bot.MonsterZone, CardId.CyberDragonHerz) + Bot.GetCountCardInZone(Bot.MonsterZone, CardId.CyberDragonNachster) + Bot.GetCountCardInZone(Bot.Hand, CardId.CyberDragon) >= 4))
+                AI.SelectCard(CardId.CyberTwinDragon);
+			else 
+				AI.SelectCard(CardId.ChimeratechRampageDragon);
+            return true;
+        }
+		
+		private bool CyberDragonHerzEffect()
+        {
+            if (!Bot.HasInHand(CardId.CyberDragonCore))
+            {
+                AI.SelectCard(CardId.CyberDragonCore);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.CyberDragon))
+            {
+                AI.SelectCard(CardId.CyberDragon);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.CyberDragonVier))
+            {
+                AI.SelectCard(CardId.CyberDragonVier);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.CyberDragonHerz))
+            {
+                AI.SelectCard(CardId.CyberDragonHerz);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.CyberDragonNachster))
+            {
+                AI.SelectCard(CardId.CyberDragonNachster);
+                return true;
+            }
+            return true;
+        }
+		
+		private bool CyberDragonCoreEffect()
+        {
+            if (!Bot.HasInHand(CardId.CyberEmergency))
+            {
+                AI.SelectCard(CardId.CyberEmergency);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.CyberRevsystem))
+            {
+                AI.SelectCard(CardId.CyberRevsystem);
+                return true;
+            }
+            return true;
+        }
+		
+		private bool CyberEmergencyEffect()
+        {
+            if (!Bot.HasInHand(CardId.CyberDragonCore))
+            {
+                AI.SelectCard(CardId.CyberDragonCore);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.CyberDragon))
+            {
+                AI.SelectCard(CardId.CyberDragon);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.CyberDragonVier))
+            {
+                AI.SelectCard(CardId.CyberDragonVier);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.CyberDragonHerz))
+            {
+                AI.SelectCard(CardId.CyberDragonHerz);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.CyberDragonNachster))
+            {
+                AI.SelectCard(CardId.CyberDragonNachster);
+                return true;
+            }
+            return true;
+        }
+		
+		private bool OverloadFusionEffect()
+        {
+			if (Duel.Turn == 1) return false;
+            if ((Bot.Graveyard.GetMatchingCardsCount(card => (card.Race & (int)CardRace.Machine) > 0) >= 0) && !Bot.HasInMonstersZone(CardId.CyberDragonSieger))
+				AI.SelectCard(CardId.ChimeratechRampageDragon);
+            return true;
+        }
+		
+        private bool CyberDragonNovaSummon()
+        {
+			AI.SelectMaterials(new List<int>() {
+                        CardId.CyberDragon,
+                        CardId.CyberDragonHerz
+            });
+            return !CyberDragonInfinitySummoned;
+        }
+
+        private bool CyberDragonNovaEffect()
+        {
+            if (ActivateDescription == Util.GetStringId(CardId.CyberDragonNova, 0))
+            {
+                return true;
+            }
+            else if (Card.Location == CardLocation.Grave)
+            {
+				AI.SelectCard(CardId.CyberEndDragon);
+                AI.SelectPosition(CardPosition.FaceUpAttack);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool CyberDragonInfinitySummon()
+        {
+            CyberDragonInfinitySummoned = true;
+            return true;
+        }
+
+        private bool CyberDragonInfinityEffect()
+        {
+            if (Duel.CurrentChain.Count > 0)
+            {
+                return Duel.LastChainPlayer == 1;
+            }
+            else
+            {
+                ClientCard bestmonster = null;
+                foreach (ClientCard monster in Enemy.GetMonsters())
+                {
+                    if (monster.IsAttack() && (bestmonster == null || monster.Attack >= bestmonster.Attack))
+                        bestmonster = monster;
+                }
+                if (bestmonster != null)
+                {
+                    AI.SelectCard(bestmonster);
+                    return true;
+                }
+            }
+            return false;
+        }
+		
+		private bool LimiterRemovalEffect()
+        {
+            if (Duel.Player == 0 && Duel.Phase == DuelPhase.BattleStart) return true;
+			else 
+				return false;
+        }
+		
+		private bool CyberDragonSiegerSummon()
+        {
+            IList<ClientCard> material_list = new List<ClientCard>();
+            foreach (ClientCard monster in Bot.GetMonsters())
+            {
+                if (monster.IsCode(CardId.CyberDragonCore, CardId.CyberDragonHerz, CardId.CyberDragonNachster, CardId.CyberDragonVier, CardId.CyberDragon))
+                    material_list.Add(monster);
+                if (material_list.Count == 2) break;
+            }
+            if (material_list.Count < 2) return false;
+            if (Bot.HasInMonstersZone(CardId.CyberDragonSieger)) return false;
+            AI.SelectMaterials(material_list);
+            return true;
+        }
+    }
+}
---
+++ Game/AI/Decks/NekrozExecutor.cs
@@ -0,0 +1,379 @@
+﻿using YGOSharp.OCGWrapper.Enums;
+using System.Collections.Generic;
+using WindBot;
+using WindBot.Game;
+using WindBot.Game.AI;
+
+namespace WindBot.Game.AI.Decks
+{
+    // NOT FINISHED YET
+    [Deck("Nekroz", "AI_Nekroz", "NotFinished")]
+    public class NekrozExecutor : DefaultExecutor
+    {
+        public class CardId
+        {
+            public const int ThousandHands = 23401839;
+            public const int TenThousandHands = 95492061;
+            public const int Shurit = 90307777;
+			public const int AshBlossomAndJoyousSpring = 14558127;
+            public const int MaxxC = 23434538;
+            public const int DecisiveArmor = 88240999;
+            public const int Trishula = 52068432;
+            public const int Valkyrus = 25857246;
+            public const int Gungnir = 74122412;
+            public const int Brionac = 26674724;
+            public const int Unicore = 89463537;
+            public const int Clausolas = 99185129;
+
+            public const int Raigeki = 12580477;
+			public const int CalledByTheGrave = 24224830;
+            public const int ReinforcementOfTheArmy = 32807846;
+            public const int PreparationOfRites = 96729612;
+            public const int Mirror = 14735698;
+            public const int Kaleidoscope = 51124303;
+            public const int Cycle = 97211663;
+            public const int EvilswarmExcitonKnight = 46772449;
+            public const int HeraldOfTheArcLight = 79606837;
+			public const int GagagaCowboy = 12014404;
+        }
+
+        List<int> NekrozRituelCard = new List<int>();
+        List<int> NekrozSpellCard = new List<int>();
+
+        public NekrozExecutor(GameAI ai, Duel duel)
+            : base(ai, duel)
+        {
+            NekrozRituelCard.Add(CardId.Clausolas);
+            NekrozRituelCard.Add(CardId.Unicore);
+            NekrozRituelCard.Add(CardId.DecisiveArmor);
+            NekrozRituelCard.Add(CardId.Brionac);
+            NekrozRituelCard.Add(CardId.Trishula);
+            NekrozRituelCard.Add(CardId.Gungnir);
+            NekrozRituelCard.Add(CardId.Valkyrus);
+
+            NekrozSpellCard.Add(CardId.Mirror);
+            NekrozSpellCard.Add(CardId.Kaleidoscope);
+            NekrozSpellCard.Add(CardId.Cycle);
+
+            AddExecutor(ExecutorType.SpellSet, DefaultSpellSet);
+            AddExecutor(ExecutorType.Repos, DefaultMonsterRepos);
+			
+            // burn if enemy's LP is below 800
+            AddExecutor(ExecutorType.SpSummon, CardId.GagagaCowboy, GagagaCowboySummon);
+            AddExecutor(ExecutorType.Activate, CardId.GagagaCowboy);
+            
+			AddExecutor(ExecutorType.Activate, CardId.CalledByTheGrave, DefaultCalledByTheGrave);
+			AddExecutor(ExecutorType.Activate, CardId.Raigeki, DefaultRaigeki);
+            AddExecutor(ExecutorType.Activate, CardId.ReinforcementOfTheArmy, ReinforcementOfTheArmyEffect);
+            AddExecutor(ExecutorType.Activate, CardId.PreparationOfRites);
+            AddExecutor(ExecutorType.Activate, CardId.Mirror);
+            AddExecutor(ExecutorType.Activate, CardId.Kaleidoscope);
+            AddExecutor(ExecutorType.Activate, CardId.Cycle);
+
+            AddExecutor(ExecutorType.MonsterSet, CardId.Shurit, ShuritSet);
+            AddExecutor(ExecutorType.Summon, CardId.ThousandHands, ThousandHandsSummon);
+            AddExecutor(ExecutorType.Summon, CardId.TenThousandHands, TenThousandHandsSummon);
+
+            AddExecutor(ExecutorType.Activate, CardId.Unicore, UnicoreEffect);
+            AddExecutor(ExecutorType.Activate, CardId.DecisiveArmor, DecisiveArmorEffect);
+            AddExecutor(ExecutorType.Activate, CardId.Valkyrus, ValkyrusEffect);
+            AddExecutor(ExecutorType.Activate, CardId.Gungnir, GungnirEffect);
+            AddExecutor(ExecutorType.Activate, CardId.Brionac, BrionacEffect);
+            AddExecutor(ExecutorType.Activate, CardId.Clausolas, ClausolasEffect);
+            AddExecutor(ExecutorType.Activate, CardId.Trishula);
+            AddExecutor(ExecutorType.Activate, CardId.EvilswarmExcitonKnight, DefaultEvilswarmExcitonKnightEffect);
+			AddExecutor(ExecutorType.Activate, CardId.AshBlossomAndJoyousSpring, DefaultAshBlossomAndJoyousSpring);
+            AddExecutor(ExecutorType.Activate, CardId.MaxxC, DefaultMaxxC);
+            AddExecutor(ExecutorType.Activate, CardId.ThousandHands, ThousandHandsEffect);
+            AddExecutor(ExecutorType.Activate, CardId.TenThousandHands, BrionacEffect);
+            AddExecutor(ExecutorType.Activate, CardId.HeraldOfTheArcLight, HeraldOfTheArcLightEffect);
+            AddExecutor(ExecutorType.Activate, CardId.Shurit, ShuritEffect);
+
+            AddExecutor(ExecutorType.SpSummon, CardId.Trishula);
+            AddExecutor(ExecutorType.SpSummon, CardId.DecisiveArmor);
+            AddExecutor(ExecutorType.SpSummon, CardId.Valkyrus);
+            AddExecutor(ExecutorType.SpSummon, CardId.Gungnir);
+            AddExecutor(ExecutorType.SpSummon, CardId.Brionac);
+            AddExecutor(ExecutorType.SpSummon, CardId.Unicore);
+            AddExecutor(ExecutorType.SpSummon, CardId.Clausolas);
+            AddExecutor(ExecutorType.SpSummon, CardId.EvilswarmExcitonKnight, DefaultEvilswarmExcitonKnightSummon);
+        }
+
+        private bool ThousandHandsSummon()
+        {
+            if (!Bot.HasInHand(NekrozRituelCard) || Bot.HasInHand(CardId.Shurit) || !Bot.HasInHand(NekrozSpellCard))
+                return true;
+            foreach (ClientCard Card in Bot.Hand)
+                if (Card != null && Card.IsCode(CardId.Kaleidoscope) && !Bot.HasInHand(CardId.Unicore))
+                    return true;
+                else if (Card.IsCode(CardId.Trishula) || Card.IsCode(CardId.DecisiveArmor) && !Bot.HasInHand(CardId.Mirror) || !Bot.HasInHand(CardId.Shurit))
+                    return true;
+            return false;
+        }
+
+        private bool ReinforcementOfTheArmyEffect()
+        {
+            if (!Bot.HasInGraveyard(CardId.Shurit) && !Bot.HasInHand(CardId.Shurit))
+            {
+                AI.SelectCard(CardId.Shurit);
+                return true;
+            }
+            return false;
+        }
+
+        private bool TenThousandHandsSummon()
+        {
+                if (!Bot.HasInHand(CardId.ThousandHands) || !Bot.HasInHand(CardId.Shurit))
+                return true;
+            return false;
+        }
+		
+        private bool ShuritSet()
+        {
+            if (!Bot.HasInHand(CardId.ThousandHands) && !Bot.HasInHand(CardId.TenThousandHands))
+                return true;
+            return false;
+        }
+
+        private bool DecisiveArmorEffect()
+        {
+            if (Util.IsAllEnemyBetterThanValue(3300, true))
+            {
+                AI.SelectCard(CardId.DecisiveArmor);
+                return true;
+            }
+            return false;
+        }
+
+        private bool ValkyrusEffect()
+        {
+            if (Duel.Phase == DuelPhase.Battle)
+                return true;
+            return false;
+        }
+
+        private bool GungnirEffect()
+        {
+            if (Util.IsOneEnemyBetter(true) && Duel.Phase == DuelPhase.Main1)
+            {
+                AI.SelectCard(Enemy.GetMonsters().GetHighestAttackMonster());
+                return true;
+            }
+            return false;
+        }
+
+        private bool BrionacEffect()
+        {
+            if (!Bot.HasInHand(CardId.Shurit))
+            {
+                AI.SelectCard(CardId.Shurit);
+                return true;
+            }
+            else if (Util.IsOneEnemyBetterThanValue(2700, true) && !Bot.HasInHand(CardId.Trishula))
+            {
+                AI.SelectCard(CardId.Trishula);
+                return true;
+            }
+            else if (Util.IsAllEnemyBetterThanValue(3300,true) && !Bot.HasInHand(CardId.DecisiveArmor))
+            {
+                AI.SelectCard(CardId.DecisiveArmor);
+                return true;
+            }
+			else if (Util.IsAllEnemyBetterThanValue(2500,true) && !Bot.HasInHand(CardId.Gungnir))
+            {
+                AI.SelectCard(CardId.Gungnir);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.Unicore) && Bot.HasInHand(CardId.Kaleidoscope))
+            {
+                AI.SelectCard(CardId.Unicore);
+                return true;
+            }
+            return true;
+        }
+		
+		private bool ShuritEffect()
+        {
+            if (!Bot.HasInHand(CardId.Brionac))
+            {
+                AI.SelectCard(CardId.Brionac);
+                return true;
+            }
+            else if (Util.IsOneEnemyBetterThanValue(3300, true) && !Bot.HasInHand(CardId.Trishula))
+            {
+                AI.SelectCard(CardId.Trishula);
+                return true;
+            }
+            return true;
+        }
+		
+		private bool HeraldOfTheArcLightEffect()
+		{
+		if (!Bot.HasInHand(CardId.Shurit))
+            {
+                AI.SelectCard(CardId.Brionac);
+                return true;
+            }
+            else if (Util.IsOneEnemyBetterThanValue(2700, true) && !Bot.HasInHand(CardId.Trishula))
+            {
+                AI.SelectCard(CardId.Brionac);
+                return true;
+            }
+            else if (Util.IsAllEnemyBetterThanValue(3300,true) && !Bot.HasInHand(CardId.DecisiveArmor))
+            {
+                AI.SelectCard(CardId.DecisiveArmor);
+                return true;
+            }
+			else if (Util.IsAllEnemyBetterThanValue(2500,true) && !Bot.HasInHand(CardId.Gungnir))
+            {
+                AI.SelectCard(CardId.Gungnir);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.Unicore) && Bot.HasInHand(CardId.Kaleidoscope))
+            {
+                AI.SelectCard(CardId.Unicore);
+                return true;
+            }
+			else if (!Bot.HasInHand(NekrozSpellCard) && !Bot.HasInHand(CardId.Unicore))
+            {
+                AI.SelectCard(CardId.Mirror);
+                return true;
+            }
+            else if (Bot.HasInHand(CardId.Unicore) && !Bot.HasInHand(CardId.Kaleidoscope))
+            {
+                AI.SelectCard(CardId.Kaleidoscope);
+                return true;
+            }
+            else 
+			{
+                AI.SelectCard(CardId.Cycle);
+                return true;
+            }
+            return true;
+        }
+
+        private bool ThousandHandsEffect()
+        {
+            if (Util.IsOneEnemyBetterThanValue(3300, true) && !Bot.HasInHand(CardId.Trishula))
+            {
+                AI.SelectCard(CardId.Trishula);
+                return true;
+            }
+            else if (Util.IsAllEnemyBetterThanValue(2700, true) && !Bot.HasInHand(CardId.DecisiveArmor))
+            {
+                AI.SelectCard(CardId.DecisiveArmor);
+                return true;
+            }
+            else if (!Bot.HasInHand(CardId.Unicore) && Bot.HasInHand(CardId.Kaleidoscope))
+            {
+                AI.SelectCard(CardId.Unicore);
+                return true;
+            }
+            return true;
+        }
+
+        private bool UnicoreEffect()
+        {
+            if (Bot.HasInGraveyard(CardId.Shurit))
+            {
+                AI.SelectCard(CardId.Shurit);
+                return true;
+            }
+            return false;
+        }
+
+        private bool ClausolasEffect()
+        {
+            if (!Bot.HasInHand(NekrozSpellCard) && !Bot.HasInHand(CardId.Unicore))
+            {
+                AI.SelectCard(CardId.Mirror);
+                return true;
+            }
+            else if (Bot.HasInHand(CardId.Unicore) && !Bot.HasInHand(CardId.Kaleidoscope))
+            {
+                AI.SelectCard(CardId.Kaleidoscope);
+                return true;
+            }
+            else
+			{
+                AI.SelectCard(CardId.Cycle);
+                return true;
+			}
+			return true;
+        }
+
+        private bool IsTheLastPossibility()
+        {
+            if (!Bot.HasInHand(CardId.DecisiveArmor) && !Bot.HasInHand(CardId.Trishula))
+                return true;
+            return false;
+        }
+		
+        private bool GagagaCowboySummon()
+        {
+            if (Enemy.LifePoints <= 800 || (Bot.GetMonsterCount() >= 4 && Enemy.LifePoints <= 1600))
+            {
+                AI.SelectPosition(CardPosition.FaceUpDefence);
+                return true;
+            }
+            return false;
+        }
+
+        private bool SelectNekrozWhoInvoke()
+        {
+            List<int> NekrozCard = new List<int>();
+            try
+            {
+                foreach (ClientCard card in Bot.Hand)
+                    if (card != null && card.IsCode(NekrozRituelCard))
+                        NekrozCard.Add(card.Id);
+
+                foreach (int Id in NekrozCard)
+                {
+                    if (Id == CardId.Trishula && Util.IsAllEnemyBetterThanValue(2700, true) && Bot.HasInHand(CardId.DecisiveArmor))
+                    {
+                        AI.SelectCard(CardId.Trishula);
+                        return true;
+                    }
+                    else if (Id == CardId.DecisiveArmor)
+                    {
+                        AI.SelectCard(CardId.DecisiveArmor);
+                        return true;
+                    }
+                    else if (Id == CardId.Unicore && Bot.HasInHand(CardId.Kaleidoscope) && !Bot.HasInGraveyard(CardId.Shurit))
+                    {
+                        AI.SelectCard(CardId.Unicore);
+                        return true;
+                    }
+                    else if (Id == CardId.Valkyrus)
+                    {
+                        if (IsTheLastPossibility())
+                        {
+                            AI.SelectCard(CardId.Valkyrus);
+                            return true;
+                        }
+                    }
+                    else if (Id == CardId.Gungnir)
+                    {
+                        if (IsTheLastPossibility())
+                        {
+                            AI.SelectCard(CardId.Gungnir);
+                            return true;
+                        }
+                    }
+                    else if (Id == CardId.Clausolas)
+                    {
+                        if (IsTheLastPossibility())
+                        {
+                            AI.SelectCard(CardId.Clausolas);
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            catch
+            { return false; }
+        }
+    }
+}
