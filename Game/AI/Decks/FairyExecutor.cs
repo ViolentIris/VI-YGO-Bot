877ad9526d4d9cd237a6eef6f0f946def5a1937d
VI-1911 <2571845421@qq.com>
2023-01-19 23:41:36 +0800

    .

--- Game/AI/Decks/FairyExecutor.cs
+++ Game/AI/Decks/FairyExecutor.cs
@@ -117,8 +117,8 @@ namespace WindBot.Game.AI.Decks
         }
 		private bool AriadneScaleActivate()
         {
-            if (Bot.HasInSpellZone(CardId.Ariadne))
-            return false;
+            if (Bot.HasInSpellZone(CardId.Ariadne)) return false;
+			return false;
         }
         private bool Solidarity()
         {
@@ -132,7 +132,7 @@ namespace WindBot.Game.AI.Decks
         }
 		private bool Paladin1Effect1()
         {           
-            foreach (ClientCard e_c in Bot.HasInHandOrInGraveyard(CardId.Honest, CardId.Statue, CardId.Ariadne, CardId.Artemis, CardId.Meltiel))
+            foreach (ClientCard e_c in Bot.Graveyard())
                     AI.SelectCard(CardId.Honest, CardId.Statue, CardId.Ariadne, CardId.Artemis, CardId.Meltiel);
 					AI.SelectNextCard(CardId.Honest, CardId.Statue, CardId.Ariadne, CardId.Artemis, CardId.Meltiel);
 					AI.SelectPosition(CardPosition.FaceUpAttack);
