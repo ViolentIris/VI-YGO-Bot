4aaa80e9835d78e7f917c64403f533af872c1e24
VI-1911 <2571845421@qq.com>
2023-01-19 18:21:30 +0800

    .

--- Game/AI/Decks/ExodiaExecutor.cs
+++ Game/AI/Decks/ExodiaExecutor.cs
@@ -168,7 +168,7 @@ namespace WindBot.Game.AI.Decks
         {
             if (Bot.SpellZone[5].IsCode(CardId.Hole) && Bot.SpellZone[5].Disabled==0) return false;
 			if (Bot.HasInHand(CardId.Chicken) || Bot.HasInHand(CardId.Terraforming)) return false;
-			if (Enemy.MonsterZone.Count >= 1) return false;
+			if (Enemy.GetMonsterCount() >= 1) return true;
             return false;
         }
 		private bool TerraformingAct()
