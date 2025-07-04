eb8c9f346b4acd7ebcdddd7c7c2880ed061aadc1
VI-1911 <2571845421@qq.com>
2025-07-04 22:21:59 +0800

    .

--- WindBotInfo.cs
+++ WindBotInfo.cs
@@ -7,7 +7,6 @@ namespace WindBot
         public string Name { get; set; }
         public string Deck { get; set; }
         public string DeckFile { get; set; }
-        public string DeckCode { get; set; }
         public string Dialog { get; set; }
         public string Host { get; set; }
         public int Port { get; set; }
@@ -21,12 +20,11 @@ namespace WindBot
             Name = "WindBot";
             Deck = null;
             DeckFile = null;
-            DeckCode = null;
             Dialog = "default";
             Host = "127.0.0.1";
             Port = 7911;
             HostInfo = "";
-            Version = 0x1361;
+            Version = 0x1362;
             Hand = 0;
             Debug = false;
             Chat = true;
